c99f4e7f465c991885f2f195c2d57d835a0d14e2
William <williamdjting@gmail.com>
2020-08-05 20:04:14 +0000

    remove assignment of const value of 1

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
   // when user has an input inside the #tweet-text area of form, minus the number of presses by the 140 character limit
   $("#tweet-text").on('input', function () {
     const numberOfPresses = $(this).val();
-    let num = 1;
+    // let num = 1;
     //minuses the number of presses by the 140 character limit
-    num = 140 - (numberOfPresses.length)
+    const num = 140 - (numberOfPresses.length)
     //counter variable finds the counter class within the form in the html
     const counter = $(this).closest('form').find('.counter');
     counter[0].textContent = num;
